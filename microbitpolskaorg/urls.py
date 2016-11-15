af360e63f3f3e818dc45c0fb23aa7d9b221c1058
Christian Barra <christianbarra@Christians-MacBook-Pro.local>
2016-11-15 02:30:11 +0100

    added dockerfiles and django app

---
+++ microbitpolskaorg/urls.py
@@ -0,0 +1,36 @@
+"""microbitpolskaorg URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from django.conf.urls.i18n import i18n_patterns
+
+import main.views as mviews
+import blog.views as bviews
+
+
+
+urlpatterns = [
+    url(r'^sitemap\.xml$', sitemap, name='django.contrib.sitemaps.views.sitemap'),
+    url(r'^grappelli/', include('grappelli.urls')),
+    url(r'^admin/', admin.site.urls),
+]
+
+
+urlpatterns += i18n_patterns(
+    url(r'^$', mviews.index, name='index'),
+    url(r'^blog/', bviews.index, name='blog'),
+)
